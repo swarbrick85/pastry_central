d73157057abb893a75749f6ade41656a998f3d24
David F Swarbrick II <39918630+swarbrick85@users.noreply.github.com>
2019-02-13 16:17:15 +0530

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,1 +1,14 @@
+class User < ActiveRecord::Base
+  has_many :pastries
 
+  def slug
+    self.username.strip.downcase.gsub /\W+/, '-'
+  end
+
+  def self.find_by_slug(slug)
+    self.find {|obj| obj.slug == slug}
+  end
+
+  has_secure_password
+
+end
