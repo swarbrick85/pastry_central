7b74cf1141c8d83e22c3e0eee42d613ba5721005
David F Swarbrick II <39918630+swarbrick85@users.noreply.github.com>
2019-02-13 16:22:23 +0530

    Update pastry.rb

--- app/models/pastry.rb
+++ app/models/pastry.rb
@@ -1,1 +1,11 @@
+class Pastry < ActiveRecord::Base
+  belongs_to :user
 
+  def slug
+    self.username.strip.downcase.gsub /\W+/, '-'
+  end
+
+  def self.find_by_slug(slug)
+    self.find {|obj| obj.slug == slug}
+  end
+end
