4642c669bed02b49066569b0a0e6331bd04e5a36
David F Swarbrick II <39918630+swarbrick85@users.noreply.github.com>
2019-02-13 17:03:01 +0530

    Update 01_create_pastries.rb

--- db/migrate/01_create_pastries.rb
+++ db/migrate/01_create_pastries.rb
@@ -3,7 +3,7 @@ class CreatePastries < ActiveRecord::Migration
     create_table :pastries do |t|
       t.string :name
       t.text :ingredients
-      t.text :added_kitchenware
+      t.text :kitchenware
       t.text :directions
       t.string :price_range
     end
