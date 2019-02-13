94cefc5b08b12e9da0903ffc66bac575feba50c6
David F Swarbrick II <39918630+swarbrick85@users.noreply.github.com>
2019-02-13 15:11:14 +0530

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,1 +1,32 @@
+require './config/environment'
 
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    
+  end
+
+  helpers do
+
+    def current_user
+      if session[:user_id]
+        @current_user = User.find_by(id: session[:user_id])
+      end
+    end
+
+    def logged_in?
+      if current_user
+        true
+      else
+        false
+      end 
+    end
+  end
+end
